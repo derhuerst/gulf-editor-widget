6bfc68d6f30e197f4c39ad2624792b8c8b796303
Jannis R <mail@jannisr.de>
2016-09-11 23:32:25 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 'use strict'
 
-// todo
+const ot = require('ot-text').type
+
+const diff = require('./diff')
+const noop = () => {}
+
+
+
+const connect = (doc, editor) => {
+	let oldContent = editor.textBuf.getText()
+
+	doc._setContents = (newContent, cb) => {
+		oldContent = newContent
+		editor.textBuf.setText(newContent)
+		cb()
+	}
+
+	doc._change = (changes, cb) => {
+		const newContent = ot.apply(oldContent, changes)
+		editor.textBuf.setText(newContent)
+		// todo: transform selection
+		cb()
+	}
+
+	doc._collectChanges = (cb) => {
+		const newContent = editor.textBuf.getText()
+		doc.update(diff(oldContent, newContent))
+		oldContent = newContent
+		cb()
+	}
+
+	editor.textBuf.onDidChange(() => {
+		doc._collectChanges(noop)
+	})
+}
+
+module.exports = connect
